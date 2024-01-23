6103eee302c26bff9633ed2625e3ad59f64771a5
MD. Tanvir Rana <134771033+tanvir-rana09@users.noreply.github.com>
2024-01-23 22:37:01 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { TodoProvider } from './Context/Context'
+import { TodoProvider } from './Context/Index'
 import './App.css'
 import TodoForm from './Components/TodoForm'
 import TodoItem from './Components/TodoItem'
